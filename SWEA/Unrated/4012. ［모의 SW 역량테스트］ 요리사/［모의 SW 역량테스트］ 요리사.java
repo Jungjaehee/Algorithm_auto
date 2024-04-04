381a5028d52b26f62a884b1adef9009356e2ce47
정재희 <wjfwogml5890@naver.com>
2024-04-04 10:16:27 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 162 ms, Memory: 27,948 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -2,77 +2,70 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-
-	static int[][] foodCombi;
-	static int N;
-	static int minDiff;
-	static int[] picked;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int TC = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		for (int test_case = 1; test_case <=TC; test_case++) {
-			sb.append("#").append(test_case).append(" ");
-			N = Integer.parseInt(br.readLine());
-			foodCombi = new int[N][N];
-			minDiff = Integer.MAX_VALUE;
-			picked = new int[N];
-			for (int i = N-1; i >= N/2; i--) picked[i] = 1;
-			
-			StringTokenizer st;
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					foodCombi[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			do {
-				int sum = 0;
-				for (int i = 0; i < N; i++) {
-					for (int j = i; j < N; j++) {
-						if(picked[i] != picked[j]) continue;
-						if(picked[j] == 1) {
-							sum += foodCombi[i][j] + foodCombi[j][i];
-						}
-						else{
-							sum -= (foodCombi[i][j] + foodCombi[j][i]);
-						}						
-					}
-				}
-				minDiff = Math.min(minDiff, Math.abs(sum));
-			}while(nextPermutation(picked));
-			
-			
-			sb.append(minDiff).append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-	static boolean nextPermutation(int[] picked) {
-		int i = N-1;
-		while(i > 0 && picked[i-1] >= picked[i]) i--;
-		
-		if(i == 0) return false;
-		
-		int j = N-1;
-		while(picked[i-1] >= picked[j]) j--;
-		swap(i-1, j, picked);
-		
-		int k = N-1;
-		while(i<k) swap(i++, k--, picked);
-		
-		return true;
-	}
-
-	private static void swap(int i, int j, int[] arr) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
-
-
+    static int[][] foodCombi;
+    static int N;
+    static int minDiff;
+    static int[] pickedA;
+    static int[] pickedB;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+         
+        for (int test_case = 1; test_case <=TC; test_case++) {
+            sb.append("#").append(test_case).append(" ");
+            N = Integer.parseInt(br.readLine());
+            foodCombi = new int[N][N];
+            minDiff = Integer.MAX_VALUE;
+            pickedA = new int[N/2];
+            pickedB = new int[N/2];
+             
+            StringTokenizer st;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    foodCombi[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+             
+            combination(0, 0);
+             
+            sb.append(minDiff).append("\n");
+        }
+        System.out.println(sb);
+    }
+    /**
+     * N개 중 N/2개를 뽑은 재귀 함수, 뽑은 후 sum계산 및 최소값 갱신
+     * @param start: 요소를 뽑을 시작 위치
+     * @param depth: 조합 뽑은 횟수
+     */
+    private static void combination(int start, int depth) {
+        if(depth == N/2) {
+            int sum = 0, idxA = 0, idxB = 0;
+            for (int i = 0; i < N; i++) {
+                if(idxA < N/2 && i==pickedA[idxA]) { 
+                    idxA++;
+                    continue;
+                }
+                pickedB[idxB++] = i;
+            }
+            for (int i = 0; i < N/2; i++) {
+                for (int j = i; j < N/2; j++) {
+                    sum += foodCombi[pickedA[i]][pickedA[j]] + foodCombi[pickedA[j]][pickedA[i]]
+                          - foodCombi[pickedB[i]][pickedB[j]] - foodCombi[pickedB[j]][pickedB[i]];
+                }
+            }
+             
+            minDiff = Math.min(minDiff, Math.abs(sum));
+            return;
+        }
+         
+        for (int i = start; i < N; i++) {
+            pickedA[depth] = i;
+            combination(i+1, depth+1);
+        }
+    }
+ 
 }
